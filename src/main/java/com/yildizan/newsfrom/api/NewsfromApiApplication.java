b1c8d9a2e2d71dbfc33c14a34061628ab9571be6
anilyildiz <anil.yildiz@outlook.com.tr>
2026-03-17 01:30:16 +0100

    added feign client for map

--- src/main/java/com/yildizan/newsfrom/api/NewsfromApiApplication.java
+++ src/main/java/com/yildizan/newsfrom/api/NewsfromApiApplication.java
@@ -3,9 +3,11 @@ package com.yildizan.newsfrom.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
+@EnableFeignClients
 public class NewsfromApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/yildizan/newsfrom/api/client/MapboxClient.java
@@ -0,0 +1,19 @@
+package com.yildizan.newsfrom.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "mapbox", url = "https://api.mapbox.com")
+public interface MapboxClient {
+
+    @GetMapping("/styles/v1/yildizan/cjzfop7z51bop1cmvns8ekqa4/tiles/256/{z}/{x}/{y}")
+    byte[] fetchTile(
+            @PathVariable("z") int z,
+            @PathVariable("x") int x,
+            @PathVariable("y") int y,
+            @RequestParam("access_token") String accessToken
+    );
+
+}
