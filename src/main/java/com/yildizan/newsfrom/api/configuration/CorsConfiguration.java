609a00a208058b3e5f26ec01ff225b359a29c3da
anilyildiz <anil.yildiz@outlook.com.tr>
2026-03-17 01:30:38 +0100

    parametrized cors config

--- src/main/java/com/yildizan/newsfrom/api/configuration/CorsConfiguration.java
+++ src/main/java/com/yildizan/newsfrom/api/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.yildizan.newsfrom.api.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration {
 
+    @Value("${api.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +19,7 @@ public class CorsConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                     .addMapping("/**")
-                    .allowedOrigins("https://newsfrom.news")
+                    .allowedOrigins(allowedOrigin)
                     .allowedMethods("GET");
             }
         };
