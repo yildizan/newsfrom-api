053fc13b8d7c17b746ce9acb3279142656ce3e87
anilyildiz <anil.yildiz@outlook.com.tr>
2026-03-17 01:30:46 +0100

    trimmed unused mapper funcs

--- src/main/java/com/yildizan/newsfrom/api/mapper/DtoMapper.java
+++ src/main/java/com/yildizan/newsfrom/api/mapper/DtoMapper.java
@@ -2,25 +2,17 @@ package com.yildizan.newsfrom.api.mapper;
 
 import com.yildizan.newsfrom.api.dto.*;
 import com.yildizan.newsfrom.api.entity.*;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DtoMapper {
 
-    CategoryDto toCategoryDto(Category category);
-
-    BaseDto toPublisherDto(Publisher publisher);
-
     FeedDto toFeedDto(Feed feed);
     List<FeedDto> toFeedDtos(List<Feed> feeds);
-
-    @Named(value = "toNewsDto")
-    NewsDto toNewsDto(BaseNews news);
-
-    @IterableMapping(qualifiedByName = "toNewsDto")
-    List<NewsDto> toNewsDtos(List<News> news);
 
     @Mappings(value = {
             @Mapping(source = "feed.category", target = "category"),
